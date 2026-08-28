121b5bc0f89292a325f8176476b13fd743e18004
trainingbudc-png <training.budc@cpird.in.th>
2026-08-28 09:42:12 +0700

    Update user.js

--- user.js
+++ user.js
@@ -241,7 +241,7 @@ function renderUserTableRows(dataList, tbodyElement) {
         tbodyElement.innerHTML += `
             <tr class="text-center align-middle">
                 <td data-label="📌 เลขรายการ" class="fw-bold text-dark">${item.reqId}</td>
-                <td data-label="📦 รายการ">${typeHtml}</td>
+                <td data-label="📦 อุปกรณ์ที่ยืม">${typeHtml}</td>
                 <td data-label="📱 เครื่องที่เตรียม" class="text-start">${idsHtml}</td>
                 <td data-label="📊 สถานะ"><span class="badge ${badgeClass} px-3 py-2 rounded-pill shadow-sm mx-auto">${displayStatus}</span></td>
             </tr>
